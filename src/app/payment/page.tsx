2c5e8c7275781c5b10a470356d017638576b7289
Richard <richardz@richardz.szalai@gmail.com>
2024-05-28 00:41:08 +0200

    button animations for payment site

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -15,27 +15,23 @@ export default function Home() {
     3:<><div className={styles.spacer}></div><div>btc</div></>
   }
   const [selected_payment, setSelectedPayment] = useState(types_of_p[0]);
-  const [previous, setPrevious] = useState(0);
 
   const btn_handler = (type_of_p : string) => {
-    console.log(type_of_p)
     switch(type_of_p) {
+      case "order_details":
+        setSelectedPayment(types_of_p[0])
+        return;
       case "cash_":
-        if(previous==1) setSelectedPayment(types_of_p[0])
-        else setSelectedPayment(types_of_p[1])
-        setPrevious(1)
+        setSelectedPayment(types_of_p[1])
         return;
       case "ccard_":
-        if(previous==2) setSelectedPayment(types_of_p[0])
-        else setSelectedPayment(types_of_p[2])
-        setPrevious(2)
+        setSelectedPayment(types_of_p[2])
         return;
       case "btc_":
-        if(previous==3) setSelectedPayment(types_of_p[0])
-        else setSelectedPayment(types_of_p[3])
-        setPrevious(3)
+        setSelectedPayment(types_of_p[3])
         return;
       default:
+        console.log("error")
         return;
     }
   }
@@ -47,7 +43,7 @@ export default function Home() {
         {selected_payment}
         <div className={styles.spacer}></div>
         <p className={styles.order_conf_text}>Please select payment type!</p>
-        <p className={styles.order_conf_text}>Total: {0} EUR</p>
+        <p className={styles.order_conf_text} onClick={() => btn_handler("order_details")}>Total: {0} EUR</p>
         <div className={styles.btns_flex}>
           <RectBtn logo_url={4} onClick={() => btn_handler("cash_")}/>
           <RectBtn logo_url={5} onClick={() => btn_handler("ccard_")}/>
