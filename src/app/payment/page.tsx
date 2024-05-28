ceac0a3c97d82f25828f235da57352c77f841e1e
Richard <richardz@richardz.szalai@gmail.com>
2024-05-29 00:58:19 +0200

    payment animations

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -7,27 +7,38 @@ import { useState, useEffect } from 'react';
 
 
 export default function Home() {
+  // prevqr means previously btc qr code was selected
+  // prevcr means previously credit card was selected
+  const [prev_qr, setPrevQR] = useState(false);
+  const [prev_cr, setPrevCR] = useState(false);
 
   const types_of_p = {
     0:<p className={styles.order_details}>Order details</p>/*tr.map(item => (<div className={styles.}><p className={styles.ord_line}>{item.itm}: {item.pri} EUR</p></div> ))*/,
     1:<><div className={styles.spacer}></div><div><p>Please pay at the counter named KIOSK with the following code:  #{1234}</p></div></>,
-    2:<><div className={styles.spacer}></div><div className={styles.ccard}><div className={styles.chip}></div><div className={styles.ccard_num}><p>0000 0000 0000 1111</p></div><div className={styles.mc_logo}></div></div></>,
-    3:<><div className={styles.spacer}></div><div>btc</div></>
+    2:<><div className={styles.spacer}></div><div className={[styles.credit_card, prev_qr ? styles.credit_card_ani_prevqr : styles.credit_card_ani].join(" ")}><div className={[styles.chip, prev_qr ? styles.chip_ani_prevqr : styles.chip_ani].join(" ")}></div><div className={[styles.credit_card_num, prev_qr ? styles.credit_card_num_ani_prevqr : styles.credit_card_num_ani].join(" ")}><p>0000 0000 0000 1111</p></div><div className={[styles.mc_logo, prev_qr ? styles.mc_logo_ani_prevqr : styles.mc_logo_ani].join(" ")}></div></div></>,
+    3:<><div className={styles.spacer}></div><div className={[styles.btc_bg, prev_cr ? styles.btc_bg_ani_prevcr : styles.btc_bg_ani].join(" ")}><div className={[styles.btc_qr, prev_cr ? styles.btc_qr_ani_prevcr : styles.btc_qr_ani].join(" ")}></div></div></>
   }
   const [selected_payment, setSelectedPayment] = useState(types_of_p[0]);
 
   const btn_handler = (type_of_p : string) => {
+    //setAnim("fade_ 1s forwards")
     switch(type_of_p) {
       case "order_details":
+        setPrevQR(false)
+        setPrevCR(false)
         setSelectedPayment(types_of_p[0])
         return;
       case "cash_":
+        setPrevQR(false)
+        setPrevCR(false)
         setSelectedPayment(types_of_p[1])
         return;
       case "ccard_":
+        setPrevCR(true)
         setSelectedPayment(types_of_p[2])
         return;
       case "btc_":
+        setPrevQR(true)
         setSelectedPayment(types_of_p[3])
         return;
       default:
